0396cc4b28def7157467828a084f346cfd78fcdf
Albert-lop <albertolopezaraguez@gmail.com>
2024-05-23 12:43:48 +0200

    2

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: alberlop <alberlop@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 19:37:15 by alberlop          #+#    #+#             */
-/*   Updated: 2024/05/23 12:43:01 by alberlop         ###   ########.fr       */
+/*   Updated: 2024/05/23 12:43:31 by alberlop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (s[i] == (char)c) //
+		if (s[i] == (char)c)
 			return ((char *)s + i);
 		i--;
 	}
